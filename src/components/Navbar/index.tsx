2607fbf546e40c97ef0eb8aa7bac508de2e9d8bc
MuhammadJunaid01 <m.junaidbkh2020@gmail.com>
2025-09-10 22:03:40 -0800

    refactor: Update Navbar component for responsive design
    - Changed visibility classes in Navbar to improve responsiveness, switching from 'lg' to 'xl' for desktop navigation and button visibility.
    - Enhanced layout consistency for better user experience across different screen sizes.

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -208,7 +208,7 @@ const Header: React.FC = () => {
           </div>
 
           {/* Desktop_Navigation */}
-          <div className="hidden lg:flex items-center justify-center space-x-10 z-[9999]">
+          <div className="hidden xl:flex items-center justify-center space-x-10 z-[9999]">
             <Link
               aria-label="Homepage"
               className="text-white hover:opacity-60 hover:text-emerald-400 font-bold transition-colors duration-300 text-center"
@@ -529,7 +529,7 @@ const Header: React.FC = () => {
                 <Button
                   variant="ghost"
                   size="icon"
-                  className="lg:hidden text-white hover:text-emerald-400"
+                  className="xl:hidden text-white hover:text-emerald-400"
                 >
                   <Menu className="h-6 w-6" />
                   <span className="sr-only">Toggle menu</span>
