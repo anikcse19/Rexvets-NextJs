5fb2f3e053fa619a292d2d5f0a43ca35cb648940
MuhammadJunaid01 <m.junaidbkh2020@gmail.com>
2025-09-15 16:38:01 +0600

    Implement weekly schedule slot fetching in WeeklySchedule component. Integrate user session management to retrieve slots based on the doctor's reference ID, and enhance error handling for API calls. Add loading state during data fetching for improved user experience.

--- src/components/Dashboard/Doctor/Overview/WeeklySchedule.tsx
+++ src/components/Dashboard/Doctor/Overview/WeeklySchedule.tsx
@@ -1,9 +1,59 @@
+"use client";
+
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { SlotStatus } from "@/lib";
+import { getWeekRange } from "@/lib/timezone";
+import { format } from "date-fns";
 import { Clock } from "lucide-react";
-import React from "react";
+import { useSession } from "next-auth/react";
+import React, { useCallback, useEffect, useState } from "react";
 
 const WeeklySchedule = () => {
+  const { data: session } = useSession();
+  const [slots, setSlots] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const refId = session?.user?.refId;
+  const getSlots = useCallback(
+    async (startDate: string, endDate: string, refId: string) => {
+      if (!refId) {
+        return;
+      }
+      try {
+        setLoading(true);
+        console.log(startDate, endDate, "vet slotsss date");
+        // const timezoneParam = timezone ? `&timezone=${encodeURIComponent(timezone)}` : '';
+        const apiUrl = `/api/appointments/slots/slot-summary/${refId}?startDate=${startDate}&endDate=${endDate}&status=${SlotStatus.ALL}`;
+        console.log("Fetching from API:", apiUrl);
+
+        const res = await fetch(apiUrl);
+
+        if (!res.ok) {
+          const errorText = await res.text();
+          console.error("API Error Response:", errorText);
+          throw new Error(`HTTP error! status: ${res.status} - ${errorText}`);
+        }
+
+        const responseData = await res.json();
+        setSlots(responseData?.data?.periods);
+        console.log("API Response Data:", responseData);
+      } catch (error: any) {
+        console.error("Error in getAvailableSlots:", error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [refId]
+  );
+  useEffect(() => {
+    if (refId) {
+      const weeklyRange = getWeekRange();
+      const startDate = format(weeklyRange.start, "yyyy-MM-dd");
+      const endDate = format(weeklyRange.end, "yyyy-MM-dd");
+      getSlots(startDate, endDate, refId);
+    }
+  }, [getSlots]);
+
   return (
     <Card className="shadow-lg border-0 bg-white">
       <CardHeader className="pb-4">
